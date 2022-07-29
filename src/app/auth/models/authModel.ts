e513493aa46369c0fa62f290fce90c7f28c2ec6b
juanxo13597 <juanxo97master@gmail.com>
2022-07-29 13:08:38 +0200

    instalar eslint y lint:fix

--- src/app/auth/models/authModel.ts
+++ src/app/auth/models/authModel.ts
@@ -1,27 +1,20 @@
-export class LoginModel{
-  constructor(
-    public email: string,
-    public password: string,
-  ){}
+export class LoginModel {
+  constructor(public email: string, public password: string) {}
 }
 
-export class AuthModel{
-  constructor(
-    public access_token: string,
-    public token_type: string,
-    public expires_in: number,
-  ){}
+export class AuthModel {
+  constructor(public access_token: string, public token_type: string, public expires_in: number) {}
 }
 
-export class RegisterModel{
+export class RegisterModel {
   constructor(
     public email: string,
     public nombre: string,
     public password: string,
     public password_confirmation: string,
-    public apellidos:string,
-    public fecha_nacimiento:Date,
-    public direccion:string,
-    public telefono:string,
-  ){}
+    public apellidos: string,
+    public fecha_nacimiento: Date,
+    public direccion: string,
+    public telefono: string
+  ) {}
 }
