271dcfd272fec30039eda3489ab49fa93c5b24c3
juanxo13597 <juanxo97master@gmail.com>
2022-07-30 13:30:28 +0200

    header and footer

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SpinnerComponent } from './components/spinner/spinner.component';
 import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [SpinnerComponent, HomeComponent],
+  declarations: [SpinnerComponent, HomeComponent, HeaderComponent, FooterComponent],
   imports: [CommonModule],
+  exports: [SpinnerComponent, HomeComponent, HeaderComponent, FooterComponent],
 })
 export class SharedModule {}
