cd966f4cb8fb000f0df7f23ea14efcf8afec9fc5
juanxo13597 <juanxo97master@gmail.com>
2022-07-31 12:26:02 +0200

    register

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { RegisterModel } from '../../models/authModel';
 import { AuthService } from '../../services/auth.service';
 
@@ -10,13 +11,23 @@ import { AuthService } from '../../services/auth.service';
 export class RegisterComponent implements OnInit {
   registerSuccess: boolean = false;
   registerError: boolean = false;
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {}
 
   register(usuario: RegisterModel) {
-    this.authService.register(usuario).subscribe(data => {
-      console.log(data);
-    });
+    this.authService.register(usuario).subscribe(
+      data => {
+        this.registerSuccess = true;
+        this.registerError = false;
+        setTimeout(() => {
+          this.router.navigate(['/auth/login']);
+        }, 3000);
+      },
+      error => {
+        this.registerError = true;
+        this.registerSuccess = false;
+      }
+    );
   }
 }
